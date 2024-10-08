6c898d1ee33960adace1a40b6325187b1987207d
Fiorella-vq <fio.89@hotmail.com>
2024-10-07 22:30:37 -0300

    arreglos

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,15 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faClock } from "@fortawesome/free-regular-svg-icons";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
+import React from "react";
+
+// Componente Home (opcional)
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  return <div></div>;
 };
 
 export default Home;
--- src/js/index.js
+++ src/js/index.js
@@ -1,23 +1,24 @@
-// Importa React en el proyecto
+// Importa React y ReactDOM
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// Incluye tus estilos en el bundle de webpack
+// Importa los estilos
 import "../styles/index.css";
 
-
+// Importa el componente del contador de segundos
 import SecondsCounter from "./component/SecondsCounter.jsx";
 
-
+// Inicializa el tiempo
 let time = 0;
 
-// se usa el "time" para que se actualice
-
+// Crea el punto de entrada de la aplicación en el DOM
 const app = ReactDOM.createRoot(document.getElementById("app"));
+
+// Renderiza el contador de segundos
 app.render(<SecondsCounter seconds={time} />);
 
-// Actualiza el componente 
+// Actualiza el componente cada segundo
 setInterval(() => {
-    time++;
-    app.render(<SecondsCounter seconds={time} />);
+  time++;
+  app.render(<SecondsCounter seconds={time} />);
 }, 1000);
